08d1b42c587e99cd65c76b5e0109cea146b859dd
Tamer Turksoy <tamer.turksoy@asisct.com>
2021-12-03 15:03:02 +0300

    first commit

---
+++ gRPCService/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gRPCService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .UseWindowsService()
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.ConfigureKestrel(options =>
+                   {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        options.ListenLocalhost(10443, listenOptions =>
+                       {
+                           listenOptions.Protocols = HttpProtocols.Http1;
+                       });
+
+                       options.ListenLocalhost(8187, listenOptions =>
+                       {
+                           listenOptions.Protocols = HttpProtocols.Http2;
+                       });
+                   });
+                   webBuilder.UseStartup<Startup>();
+               });
+    }
+}
